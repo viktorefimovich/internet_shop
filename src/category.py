7017af5e8e9ff065713e5137bee71e12fd35ce6e
viktorefimovich <viktorefimovich@bk.ru>
2024-08-30 13:23:58 +0700

    add category_count, product_count in class Category

--- src/category.py
+++ src/category.py
@@ -4,8 +4,12 @@ class Category:
     name: str
     description: str
     products: list
+    category_count = 0
+    product_count = 0
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.products = products if products else []
+        Category.category_count += 1
+        Category.product_count = len(products) if products else 0
