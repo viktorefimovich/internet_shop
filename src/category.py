db95eb6f246d1c93953e942d813f2ac7e49c43e2
viktorefimovich <viktorefimovich@bk.ru>
2024-09-02 11:45:03 +0700

    add getter products in category.py add method add_product in category.py

--- src/category.py
+++ src/category.py
@@ -1,9 +1,12 @@
+from src.product import Product
+
+
 class Category:
     """Класс для представления категорий"""
 
     name: str
     description: str
-    products: list
+    __products: list
     category_count = 0
     product_count = 0
 
@@ -12,6 +15,21 @@ class Category:
 
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+    @property
+    def products(self) -> str:
+        """Геттер для вывода списка товаров в виде строки"""
+
+        result = ""
+        for product in self.__products:
+            result += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return result
+
+    def add_product(self, product: Product) -> None:
+        """Метод для добавления товаров в категорию"""
+
+        self.__products.append(product)
+        Category.product_count += 1
