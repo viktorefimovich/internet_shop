dc4466845ad8b4328ce16e470ff96de018309b87
viktorefimovich <viktorefimovich@bk.ru>
2024-09-03 17:25:30 +0700

    add test on class ProductIterator

---
+++ tests/test_product_iterator.py
@@ -0,0 +1,13 @@
+import pytest
+
+from src.product_iterator import ProductIterator
+
+
+def test_product_iterator(product_iterator: ProductIterator) -> None:
+    iter(product_iterator)
+    assert product_iterator.index == 0
+    assert next(product_iterator).name == "Samsung Galaxy S23 Ultra"
+    assert next(product_iterator).name == "Iphone 15"
+    assert next(product_iterator).name == "Xiaomi Redmi Note 11"
+    with pytest.raises(StopIteration):
+        next(product_iterator)
