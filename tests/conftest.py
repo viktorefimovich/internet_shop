aae9949d84c10a07bfe8187ac39031b5645e9c50
viktorefimovich <viktorefimovich@bk.ru>
2024-09-05 10:04:39 +0700

    fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -126,27 +126,27 @@ def product_iterator(category1: Category) -> ProductIterator:
 
 
 @pytest.fixture
-def smartphone1():
+def smartphone1() -> Smartphone:
     return Smartphone(
         "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5, "S23 Ultra", 256, "Серый"
     )
 
 
 @pytest.fixture
-def smartphone2():
+def smartphone2() -> Smartphone:
     return Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
 
 
 @pytest.fixture
-def smartphone3():
+def smartphone3() -> Smartphone:
     return Smartphone("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14, 90.3, "Note 11", 1024, "Синий")
 
 
 @pytest.fixture
-def grass1():
+def grass1() -> LawnGrass:
     return LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
 
 
 @pytest.fixture
-def grass2():
+def grass2() -> LawnGrass:
     return LawnGrass("Газонная трава 2", "Выносливая трава", 450.0, 15, "США", "5 дней", "Темно-зеленый")
